f73ba9dafcd215d2c6cc433ce3a8bafc4f1e907d
Omar Sahyoun <osahyoun@gmail.com>
2017-04-08 20:05:09 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,128 @@
+const template = require('lodash.template');
+
+const host = 'https://api2.bitsalad.co';
+
+const toQueryString = (opts) => {
+  const reducer = (a, k) => {
+    const part = `${k}=${encodeURIComponent(opts[k])}`;
+
+    a.push(part);
+    return a;
+  };
+
+  return Object.keys(opts).reduce(reducer, []).join('&');
+};
+
+const fetch = (url, params, onSuccess) => {
+  url = `${url}?${toQueryString(params)}`;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', url);
+  xhr.responseType = 'json';
+
+  xhr.onload = () => {
+    onSuccess(xhr);
+  };
+
+  xhr.send();
+};
+
+const defaultTemplate = `
+  <div class='bitsalad-img'>
+    <% if(options.links) { %>
+      <a href="<%= link%>" target='_blank'>
+        <img src="<%= url %>"  width="<%= width %>" height="<%= height %>" />
+      </a>
+    <% } else { %>
+        <img src="<%= url %>"  width="<%= width %>" height="<%= height %>" />
+    <% } %>
+
+    <% if(options.comments || options.likes) {%>
+      <div class='bitsalad-counters'>
+        <% if(options.likes) {%>
+          <span class='bitsalad-likes'>
+            <%= likes.count %>
+          </span>
+        <% } %>
+        <% console.log(options.tags) %>
+        <% if(options.tags) { %>
+          <span class='bitsalad-tags'>
+            <%= tags.join(', ') %>
+          </span>
+        <% } %>
+        <% if(options.comments) { %>
+          <span class='bitsalad-comments'>
+            <%= comments.count %>
+          </span>
+        <% } %>
+      </div>
+    <% } %>
+
+    <% if(options.captions && caption) { %>
+      <div class='bitsalad-caption'>
+        <%= caption.text %>
+      </div>
+    <% } %>
+  </div>
+`;
+
+const handleResponse = (data, opts) => {
+  const div = document.getElementById(opts.id);
+
+  if (!div) return;
+
+  const html = data.map((image) => {
+    let data = image.images[opts.resolution];
+
+    image = Object.assign(image, {
+      url: data.url,
+      height: data.height,
+      width: data.width,
+      options: {
+        links: opts.links,
+        likes: opts.likes,
+        comments: opts.comments,
+        captions: opts.captions,
+        tags: opts.tags
+      }
+    });
+
+    return opts.cachedTemplate(image);
+  });
+
+  div.insertAdjacentHTML('beforeend', html.join(''));
+};
+
+const defaultOptions = {
+  resolution: 'thumbnail',
+  links: false,
+  captions: false,
+  sortBy: 'created_time',
+  likes: false,
+  tags: false,
+  comments: false,
+  template: defaultTemplate,
+  filterByTags: []
+};
+
+const SaladSpinner = (opts) => {
+  opts = Object.assign({}, defaultOptions, opts);
+  opts.cachedTemplate = template(opts.template);
+
+  const params = (({feeds, sortBy}) => ({feeds, sortBy}))(opts);
+
+  if (opts.filterByTags.length) params.filterByTags = opts.filterByTags;
+
+  return {
+    show() {
+      let url = `${host}/feeds/${opts.userId}`;
+      const handleSuccess = (xhr) => {
+        handleResponse(xhr.response, opts);
+      };
+
+      fetch(url, params, handleSuccess);
+    }
+  };
+};
+
+export default SaladSpinner;
